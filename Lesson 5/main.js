2836a24974c3cd4a22c9166bdb242f84e9e85f12
Serhii <seryogamuzig@gmail.com>
2019-06-27 17:33:13 +0300

    'money meeper' app complite

---
+++ Lesson 5/main.js
@@ -0,0 +1,23 @@
+let startBtn = document.querySelector('#start'),
+    budgetValue = document.querySelector('.budget-value'),
+    dayBudgetValue = document.querySelector('.daybudget-value'),
+    levelValue = document.querySelector('.level-value'),
+    expensesValue = document.querySelector('.expenses-value'),
+    optionalExpensesValue = document.querySelector('.optionalexpenses-value'),
+    incomeValue = document.querySelector('.income-value'),
+    monthSavingsValue = document.querySelector('.monthsavings-value'),
+    yearSavingsValue = document.querySelector('.yearsavings-value'),
+
+    expensesItems = document.querySelectorAll('.expenses-item'),
+    expensesBtn = document.getElementsByTagName('button')[0],
+    optionalExpensesBtn = document.getElementsByTagName('button')[1],
+    countBtn = document.getElementsByTagName('button')[2],
+
+    optionalExpensesItem = document.querySelectorAll('.optionalexpenses-item'),
+    incomeItem = document.querySelector('.choose-income'),
+    checkSavings = document.querySelector('#savings'),
+    sumValue = document.querySelector('.choose-sum'),
+    percentValue = document.querySelector('.choose-percent'),
+    yearValue = document.querySelector('.year-value'),
+    monthValue = document.querySelector('.month-value'),
+    dayValue = document.querySelector('.day-value');
--- theoryScript.js
+++ theoryScript.js
@@ -30,3 +30,42 @@
 // document.body.insertBefore(div, circle[1]);
 
 // console.log(div);
+
+// lesson 6
+
+let btn = document.querySelectorAll('button'),
+	wrap = document.querySelector('.wrapper'),
+	link = document.querySelector('a');
+
+// btn[0].onclick = function() {
+//     alert('You pushed first button');
+// };
+
+// btn[0].onclick = function() {
+//     alert('You pushed first button again');
+// };
+
+// btn[0].addEventListener('click', function (event) {
+// 	console.log('Event: ' + event.type + ' with element ' + event.target);
+// });
+
+// wrap.addEventListener('click', function() {
+// 	console.log('Event: ' + event.type + ' with element ' + event.target);
+// });
+
+link.addEventListener('click', function(event){
+	event.preventDefault();
+	console.log('Event: ' + event.type + ' with element ' + event.target);
+});
+
+btn.forEach(function(item) {
+	item.addEventListener('mouseleave', function() {
+		console.log('Exit!');
+	});
+});
+
+
+
+// btn[0].addEventListener('mouseenter', function() {
+//     alert('You hovered on the first button');
+// });
