e381e49d748fa22d1ce9aaae7bb5440af884951a
Serhii <seryogamuzig@gmail.com>
2019-07-10 15:56:54 +0300

    options.js updated

--- Lesson 8/js/options.js
+++ Lesson 8/js/options.js
@@ -19,24 +19,3 @@ class Options {
 const item = new Options(150, 750, "lightpink", 14, "center");
 
 item.newDiv();
-
-// class Options {
-//     constructor(height, width, bg, fontSize, textAlign) {
-//         this.height = height;
-//         this.width = width;
-//         this.bg = bg;
-//         this.fontSize = fontSize;
-//         this.textAlign = textAlign;
-//     }
-//     createDiv() {
-//         let elem = document.createElement('div');
-//         document.body.appendChild(elem);
-//         let param = `height:${this.height}px; width:${this.width}px; background-color:${this.bg}; font-size:${this.fontSize}px; text-align:${this.textAlign}`;
-//         elem.style.cssText = param;
-//         elem.textContent = 'New element "div"';
-//     }
-// }
-
-// const item = new Options(200, 750, "lightpink", 25, "center");
-
-// item.createDiv();
