2dfb2d524bba726f2bc1b36d113d1b9954dcebda
Serhii <seryogamuzig@gmail.com>
2019-07-10 15:52:11 +0300

    Lesson 8 added/updated files

---
+++ Lesson 8/js/options.js
@@ -0,0 +1,42 @@
+class Options {
+    constructor(height, width, bg, fontSize, textAlign) {
+        this.height = height;
+        this.width = width;
+        this.bg = bg;
+        this.fontSize = fontSize;
+        this.textAlign = textAlign;
+    }
+    newDiv() {
+        let elem = document.createElement('div');
+        document.body.appendChild(elem);
+        let param = `height:${this.height}px; width:${this.width}px; background-color:${this.bg}; font-size:${this.fontSize}px; text-align:${this.textAlign}`;
+        elem.style.cssText = param;
+        elem.textContent = 'New "div" element';
+    }
+    
+}
+
+const item = new Options(150, 750, "lightpink", 14, "center");
+
+item.newDiv();
+
+// class Options {
+//     constructor(height, width, bg, fontSize, textAlign) {
+//         this.height = height;
+//         this.width = width;
+//         this.bg = bg;
+//         this.fontSize = fontSize;
+//         this.textAlign = textAlign;
+//     }
+//     createDiv() {
+//         let elem = document.createElement('div');
+//         document.body.appendChild(elem);
+//         let param = `height:${this.height}px; width:${this.width}px; background-color:${this.bg}; font-size:${this.fontSize}px; text-align:${this.textAlign}`;
+//         elem.style.cssText = param;
+//         elem.textContent = 'New element "div"';
+//     }
+// }
+
+// const item = new Options(200, 750, "lightpink", 25, "center");
+
+// item.createDiv();
--- theoryScript.js
+++ theoryScript.js
@@ -88,34 +88,213 @@
 //     setTimeout(log, 2000);
 // }, 2000);
 
-let btn = document.querySelector('.btn'),
-elem = document.querySelector('.box');
+// let btn = document.querySelector('.btn'),
+// elem = document.querySelector('.box');
 
-function myAnimation () {
-    let pos = 0;
+// function myAnimation () {
+//     let pos = 0;
 
-    let id = setInterval(frame, 10);
+//     let id = setInterval(frame, 10);
 
-    function frame() {
-        if (pos == 300) {
-            clearInterval(id);
-        } else {
-            pos++;
-            elem.style.top = pos + 'px';
-            elem.style.left = pos + 'px';
-        }
+//     function frame() {
+//         if (pos == 300) {
+//             clearInterval(id);
+//         } else {
+//             pos++;
+//             elem.style.top = pos + 'px';
+//             elem.style.left = pos + 'px';
+//         }
+//     }
+// }
+
+// btn.addEventListener('click', myAnimation);
+
+// let btnBlock = document.querySelector('.btn-block'),
+// btns = document.getElementsByTagName('button');
+
+// btnBlock.addEventListener('click', function(event) {
+//     // if (event.target && event.target.tagName == 'BUTTON') {
+//     // if (event.target && event.target.classList.contains('first')) {
+//     if (event.target && event.target.matches('button.first')) {
+//         console.log('check');
+//     }
+// });
+
+// new Obj
+
+// function Calculator() {
+//     this.read = function() {
+//         this.a = +prompt("a?", ""),
+//         this.b = +prompt("b?", "");
+//     }
+//     this.sum = function() {
+//         alert(this.a + this.b);
+//     }
+//     this.mult = function() {
+//         alert(this.a * this.b);
+//     }
+// }
+
+// let exmpl = new Calculator();
+
+// exmpl.read();
+// exmpl.sum();
+// exmpl.mult();
+
+// function CoffeeMachine(power) {
+//     this.power = power;
+//     this.waterAmount = 0;
+// }
+
+// CoffeeMachine.prototype.WaterHeatCapacity = 4200;
+
+// CoffeeMachine.prototype.getTimeBoil = function() {
+//     return this.waterAmount * this.WaterHeatCapacity * 80 / this.power;
+// };
+
+// CoffeeMachine.prototype.run = function() {
+//     setTimeout(function() {
+//         alert("Here is your coffee!");
+//     }, this.getTimeBoil());
+// };
+
+// CoffeeMachine.prototype.setWaterAmount = function(amount) {
+//     this.waterAmount = amount;
+// };
+
+// let coffeeMachine = new CoffeeMachine(10000);
+// coffeeMachine.setWaterAmount(50);
+// coffeeMachine.run();
+
+// les 25
+
+// let box = document.querySelector('.box'),
+//     btn = document.querySelector('button');
+
+// let width = box.scrollWidth,
+//     height = box.scrollHeight;
+
+// console.log(width);
+// console.log(height);
+// console.log(box.getBoundingClientRect());
+
+// console.log(document.documentElement.clientWidth);
+// console.log(document.documentElement.clientHeight);
+
+
+// btn.addEventListener('click', function() {
+//     //box.style.height = box.scrollHeight + 'px';
+//     box.scrollTop = 0;
+// });
+
+// constructor
+
+// function User(name, id) {
+//     this.name = name;
+//     this.id = id;
+//     this.human = true;
+//     this.hello = function() {
+//         console.log('Hello! ' + this.name);
+//     };
+// }
+// User.prototype.exit = function(name) {
+//     console.log('User ' + this.name + ' gone');
+// }
+
+// let Rajesh = new User('Rajesh', 25),
+// alex = new User('Alex', 23);
+
+// console.log(Rajesh);
+// console.log(alex);
+
+// Rajesh.exit();
+
+// Class
+
+// class User {
+//     constructor(id, name) {
+//         this.name = name;
+//         this.id = id;
+//         this.human = true;
+//     }
+//     hello() {
+//         console.log(`Hello! ${this.name}`);
+//     }
+//     exit() {
+//         console.log(`User ${this.name} has gone`);
+//     }
+// }
+
+// let jack = new User('Jack', 25);
+// let steve = new User('Steve', 27);
+
+// console.log(jack);
+// console.log(steve);
+// jack.exit();
+// steve.hello();
+
+// let obj = {
+//     a: 20,
+//     b: 15,
+//     sum: function() {
+//         console.log(this);
+//         function shout() {
+//             console.log(this);
+//         }
+//         shout();
+//     }
+// };
+// obj.sum();
+
+// es6
+
+// let name = 'Jack',
+// age = 30,
+// mail = 'exmpl@gmail.com';
+
+// //document.write('Пользователю ' + name + ' ' + age + ' лет. Его ел. адрес: ' + mail);
+// document.write(`
+// Пользователю ${name} ${age} лет. Его ел. адрес: ${mail}
+// <h1>Hello</h1>
+// <button>Don't push me</button>
+// `);
+
+//spread operator
+
+// let video = ['youtube', 'vimeo', 'rutube'],
+// blogs = ['wordpress', 'livejournal', 'blogger'],
+// internet = [...video, ...blogs, 'vk', 'facebook'];
+
+// console.log(internet);
+
+// function log (a, b, c) {
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+//     console.log(a + b + c);
+// }
+
+// let numbers = [2, 5, 7];
+
+// log(...numbers);
+
+class Options {
+    constructor(height, width, bg, fontSize, textAlign) {
+        this.height = height;
+        this.width = width;
+        this.bg = bg;
+        this.fontSize = fontSize;
+        this.textAlign = textAlign;
+    }
+    createDiv() {
+        let elem = document.createElement('div');
+        document.body.appendChild(elem);
+        let param = `height:${this.height}px; width:${this.width}px; background-color:${this.bg}; font-size:${this.fontSize}px; text-align:${this.textAlign}`;
+        elem.style.cssText = param;
+        elem.textContent = 'New element "div"';
     }
 }
 
-btn.addEventListener('click', myAnimation);
+const item = new Options(200, 750, "lightpink", 25, "center");
 
-let btnBlock = document.querySelector('.btn-block'),
-btns = document.getElementsByTagName('button');
-
-btnBlock.addEventListener('click', function(event) {
-    // if (event.target && event.target.tagName == 'BUTTON') {
-    // if (event.target && event.target.classList.contains('first')) {
-    if (event.target && event.target.matches('button.first')) {
-        console.log('check');
-    }
-});
+item.createDiv();
